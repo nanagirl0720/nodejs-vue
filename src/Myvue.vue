a4cf6da23ea96cca822afb40bb32fd3d9ebf797a
pxn <pu6422534@163.com>
2020-03-30 13:15:05 +0800

    vue-router

---
+++ src/Myvue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>nihao</h1>{{message }}
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Myvue",
+        data(){
+            return{
+                message: "zhongguo"
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
